b3eabb542d8ce59801ab1d3348321bec241574bd
Armin Schreger <armin.schreger@toscom.at>
2017-02-26 22:03:34 +0100

    Initial commit, Add first butler spec

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source 'https://rubygems.org'
+
+gem 'bundler'
+gem 'guard-rspec'
+gem 'rspec'
+gem 'libnotify'
---
+++ spec/butler_spec.rb
@@ -0,0 +1,8 @@
+RSpec.describe "Butler" do
+  describe '#list' do
+    it 'returns a list of available commands' do
+      expect(Butler.new.list).to eq ['command1', 'command2', 'command3']
+    end
+  end
+end
+
