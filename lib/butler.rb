2958620d0b5244d3696f9582c310a35306785868
Armin Schreger <armin.schreger@toscom.at>
2018-02-26 22:58:45 +0100

    Add execution method with pseudo-http return codes

--- lib/butler.rb
+++ lib/butler.rb
@@ -2,4 +2,12 @@ class Butler
   def list
     ['command1', 'command2', 'command3']
   end
+
+  def execute(task)
+    if self.list.include?(task)
+      200
+    else
+      404
+    end
+  end
 end
--- spec/butler_spec.rb
+++ spec/butler_spec.rb
@@ -5,6 +5,14 @@ RSpec.describe "Butler" do
     it 'returns a list of available commands' do
       expect(Butler.new.list).to eq ['command1', 'command2', 'command3']
     end
+
+    it 'can execute a predefined command' do
+      expect(Butler.new.execute 'command1' ).to eq(200)
+    end
+
+    it 'returns 404 if a predefined command is not built in' do
+     expect(Butler.new.execute 'command_not_found').to eq(404)
+    end 
   end
 end
 
